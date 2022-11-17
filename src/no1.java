f3d2fd9d067bfc65b9b5335fd8a68bdc34bcf32b
ijlalWindhi <ijlalwindhi15@gmail.com>
2022-11-17 09:17:06 +0700

    refactor code

--- src/no1.java
+++ src/no1.java
@@ -16,27 +16,20 @@ public class no1 {
 
     public static String pyramidCharacter(String kata) {
         String array[] = new String[kata.length()];
-
         for (int i = 0; i < kata.length(); i++) {
-
             String words = reverse(kata).substring(0, i + 1);
             words = i % 2 == 0 ? reverse(words) : words;
-
             array[i] = words;
-
             for (int j = kata.length(); words.length() <= j; j--) {
                 System.out.print(" ");
             }
-
             System.out.println(array[i]);
         }
-
         return kata;
     }
 
     public static void main(String[] args) {
         String kata = "3oz4spoon";
-
         pyramidCharacter(kata);
     }
 }
